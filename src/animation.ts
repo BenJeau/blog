5153099b42e9b2482af76d086b4ecb5cf25ac880
Benoit Jeaurond <benoit@jeaurond.dev>
2024-09-15 18:13:11 -0400

    feat: create appear animation

---
+++ src/animation.ts
@@ -0,0 +1,18 @@
+const appearAnimation = {
+  old: {
+    name: "appear",
+    duration: "0.3s",
+    easing: "ease-out",
+    direction: "reverse",
+  },
+  new: {
+    name: "appear",
+    duration: "0.3s",
+    easing: "ease-out",
+  },
+};
+
+export const appear = {
+  forwards: appearAnimation,
+  backwards: appearAnimation,
+};
